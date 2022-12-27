107238a54e128bad03b80f29b5e022ad0c8896f7
Hansika12 <108214219+Hansika12@users.noreply.github.com>
2022-12-27 14:48:28 +0000

    Add files via upload

---
+++ tools_for_data_science.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+#  # My Jupyter Notebook on IBM Watson Studio
+
+# **Hansika** <br>
+# Student
+
+# _Data science is interesting because it forms a link between data and its relevance in real world_
+
+# ### The code in the cell below should add two numbers
+
+# In[2]:
+
+
+2+2
+
+
+# 1. First 
+# 2. Second
+# 3. Third
+# 
+# - First Bullet
+# - Second Bullet
+# 
+# 
+# ***
+
+# In[ ]:
+
+
+
+
